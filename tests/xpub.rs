9790afd310555246330f726b9a597107e12d52a8
YuWeiWu <wusyong9104@gmail.com>
2020-04-03 21:20:27 +0800

    Add simple xpub test

---
+++ tests/xpub.rs
@@ -0,0 +1,22 @@
+use async_zmq::{xpublish, subscribe, Result, StreamExt, Context};
+
+#[async_std::test]
+async fn xpublish_subscribe() -> Result<()> {
+    let uri = "inproc://xpub_xsub";
+    let context = Context::new();
+    let mut xpublish = xpublish(uri)?.with_context(&context).bind()?;
+    let subscribe = subscribe(uri)?.with_context(&context).connect()?;
+
+    let topic = "Topic";
+    subscribe.set_subscribe(topic)?;
+
+    // It uses StreamExt trait method here simply we want to make sure `MessageBuf` works.
+    // It probably makes more sense to call the method from raw zmq socket.
+    let event = xpublish.next().await.unwrap().unwrap();
+    assert_eq!(&event[0][0], &1);
+
+    let expected = std::str::from_utf8(&event[0][1..]).unwrap();
+    assert_eq!(expected, topic);
+
+    Ok(())
+}
